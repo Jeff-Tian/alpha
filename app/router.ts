dccb6ef72edee43e48762697327871c5c456559b
Jeff Tian <jie.tian@unicareer.com>
2019-06-12 19:22:07 +0800

    :loud_sound: show code

--- app/router.ts
+++ app/router.ts
@@ -1,4 +1,4 @@
-import {Application} from 'egg'
+import {Application, Context} from 'egg'
 
 export default (app: Application) => {
   const {controller, router} = app
@@ -10,6 +10,9 @@ export default (app: Application) => {
   app.passport.mount('github', app.config.passportGithub)
   app.passport.mount('wechat', app.config.passportWechat)
   app.passport.mount('citi', app.config.passportCiti)
+  router.get('/passport/citi/callback', (ctx: Context) => {
+    ctx.body = ctx.query
+  })
 
   app.router.get('/logout', 'user.logout')
 }
