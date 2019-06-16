9c4ecdd0daeeb0067190344b4964aaeef8c4bdd4
Jeff Tian <jeff.tian@outlook.com>
2019-06-16 19:40:01 +0800

    :sparkles: add schedule to keep site alive

---
+++ app/schedule/keep-alive.ts
@@ -0,0 +1,24 @@
+import { Subscription } from 'egg'
+
+export default class KeepAlive extends Subscription {
+    static get schedule() {
+        return {
+            interval: '29m',
+            type: 'all'
+        }
+    }
+
+    async subscribe() {
+        const result = await this.ctx.curl('https://uniheart.herokuapp.com', {
+            // 自动解析 JSON response
+            dataType: 'json',
+            timeout: 10000
+        });
+
+        this.ctx.body = {
+            status: result.status,
+            headers: result.headers,
+            data: result.data,
+        };
+    }
+}
