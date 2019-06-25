4ab38b905f7a49fbfbd6348637c3c5b161adc4ba
Jeff Tian <jie.tian@unicareer.com>
2019-06-25 17:54:21 +0800

    :card_file_box: add primary key in model

--- app/model/authorization.ts
+++ app/model/authorization.ts
@@ -4,8 +4,8 @@ export default (app: Application) => {
   const {STRING, INTEGER, DATE, TEXT} = app.Sequelize
 
   return app.model.define('authorization', {
-    provider: STRING,
-    uid: STRING,
+    provider: {type: STRING, primaryKey: true},
+    uid: {type: STRING, primaryKey: true},
     user_id: INTEGER,
     created_at: DATE,
     updated_at: DATE,
