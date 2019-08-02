987a297b35306da810205430a3342909a7460b87
Jeff Tian <jie.tian@unicareer.com>
2019-08-02 15:43:45 +0800

    :sparkles: add passportWechatMiniProgramHardway

--- app/validate/GetAccessTokenRequest.ts
+++ app/validate/GetAccessTokenRequest.ts
@@ -3,6 +3,7 @@ import {IsEnum, IsOptional} from 'class-validator'
 export enum KeySecretSelection {
   PASSPORT_HARDWAY = 'passportHardway',
   PASSPORT_WECHAT = 'passportWechat',
+  PASSPORT_WECHAT_MINI_PROGRAM_HARDWAY = 'passportWechatMiniProgramHardway',
   CUSTOMIZED = 'customized',
 }
 
