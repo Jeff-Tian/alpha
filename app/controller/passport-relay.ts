a1efde3a056ce9a2f9c1e9af609970280c6acb98
Jeff Tian <jie.tian@unicareer.com>
2019-09-10 13:46:14 +0800

    feat: remember cache for /passport/citi

--- app/controller/passport-relay.ts
+++ app/controller/passport-relay.ts
@@ -6,20 +6,17 @@ export default class PassportRelayController extends Controller {
   public async relay() {
     const {ctx} = this
 
-    return (ctx.body = ctx.user.id)
+    const referer = await ctx.app.refererCache.get(ctx.query.state)
+    await ctx.app.refererCache.delete(ctx.query.state)
 
-    // tslint:disable-next-line:no-commented-code
-    // const referer = await ctx.app.refererCache.get(ctx.query.state)
-    // await ctx.app.refererCache.delete(ctx.query.state)
-    //
-    // if (referer) {
-    //   ctx.redirect(
-    //     referer +
-    //       (referer.indexOf('?') > 0 ? '&' : '?') +
-    //       querystring.stringify({token: ctx.user.id})
-    //   )
-    // } else {
-    //   ctx.body = {token: ctx.user.id}
-    // }
+    if (referer) {
+      ctx.redirect(
+        referer +
+          (referer.indexOf('?') > 0 ? '&' : '?') +
+          querystring.stringify({token: ctx.user.id})
+      )
+    } else {
+      ctx.body = {token: ctx.user.id}
+    }
   }
 }
