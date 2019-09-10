2dd2575755e91e54acbacba4d59b4c6ed7675d02
Jeff Tian <jie.tian@unicareer.com>
2019-09-10 19:17:51 +0800

    fix: log referer

--- app/middleware/rememberReferer.ts
+++ app/middleware/rememberReferer.ts
@@ -4,6 +4,7 @@ export default async (ctx, next) => {
   ctx.logger.info('passport started: ', {
     query: ctx.query,
     traceId: ctx.traceId,
+    referer,
   })
 
   await ctx.app.refererCache.save(ctx.query.state, referer)
