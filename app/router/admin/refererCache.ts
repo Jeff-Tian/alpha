c0cb72a79cb7a5eb6e7db8202c76561a5fc4aae8
Jeff Tian <jeff.tian@outlook.com>
2019-09-13 22:05:30 +0800

    test

--- app/router/admin/refererCache.ts
+++ app/router/admin/refererCache.ts
@@ -8,4 +8,10 @@ export default (app: Application) => {
     subRouter.get('/refererCache', async (ctx) => {
         ctx.body = { size: app.refererCache.size, [ctx.query.traceId]: await app.refererCache.get(ctx.query.traceId) }
     });
+
+    subRouter.get('/test', async (ctx) => {
+        ctx.app.refererCache.save('1234', '4321')
+
+        ctx.body = { size: app.refererCache.size, [ctx.query.traceId]: await app.refererCache.get(ctx.query.traceId) }
+    })
 }
