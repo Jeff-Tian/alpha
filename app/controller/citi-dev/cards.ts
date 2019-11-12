be0c2580695ce9de6cad69f8634bc3f1254bbd16
Jeff Tian <jie.tian@unicareer.com>
2019-11-12 17:00:39 +0800

    fix: json.parse

--- app/controller/citi-dev/cards.ts
+++ app/controller/citi-dev/cards.ts
@@ -1,9 +1,9 @@
 import CitiOAuth from 'citi-oauth'
-import { Controller } from 'egg'
+import {Controller} from 'egg'
 
 export default class CardsController extends Controller {
   public async getList() {
-    const { ctx } = this
+    const {ctx} = this
 
     const citiOAuth = new CitiOAuth(
       ctx.citiOAuthOptions.key,
@@ -15,9 +15,9 @@ export default class CardsController extends Controller {
     )
 
     const user = await ctx.service.user.get(ctx.state.user, 'citi')
-    ctx.logger.info('getting cards for ', { user })
     const accessToken = await citiOAuth.getToken(user.uid)
-    ctx.logger.info('the user\'s accessToken is ', { accessToken })
-    ctx.body = await citiOAuth.Cards.getCards(accessToken.access_token)
+    ctx.body = await citiOAuth.Cards.getCards(
+      JSON.parse(accessToken).access_token
+    )
   }
 }
