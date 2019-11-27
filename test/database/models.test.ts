ff74e56394df5033ea8b319e7839da508272e193
Jeff Tian <jie.tian@unicareer.com>
2019-11-27 17:48:58 +0800

    test models

---
+++ test/database/models.test.ts
@@ -0,0 +1,42 @@
+import assert = require('assert')
+import mm from 'egg-mock'
+import path from 'path'
+import runscript = require('runscript')
+
+const baseDir = path.resolve(__dirname, '../..')
+
+describe('test/models.test.ts', () => {
+  before(async () => {
+    await runscript('ets', {cwd: baseDir})
+    await runscript(`tsc -p ${baseDir}/tsconfig.json`, {cwd: baseDir})
+  })
+
+  after(async () => {
+    await runscript('ets clean', {cwd: baseDir})
+  })
+
+  describe('compiling code', () => {
+    afterEach(mm.restore)
+
+    let app
+
+    before(async () => {
+      app = mm.app({
+        baseDir: '../..',
+      })
+
+      return app.ready()
+    })
+
+    after(async () => {
+      app.close()
+
+      assert.deepStrictEqual(app._app._backgroundTasks, [])
+    })
+
+    it('should show models', () => {
+      assert(app.model.config.database === 'alpha')
+      assert(typeof app.model.models.authorization === 'function')
+    })
+  })
+})
