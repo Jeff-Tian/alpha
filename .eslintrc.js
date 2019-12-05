c412c6e0cca2ae8d9b3f5ec276f9a41bf29db0ce
Jeff Tian <jie.tian@unicareer.com>
2019-12-05 20:59:25 +0800

    fix: upload missing files

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: 'eslint-config-egg/typescript',
+  // parser: 'babel-eslint',
+  parserOptions: {
+    // recommend to use another config file like tsconfig.eslint.json and extends tsconfig.json in it.
+    // because you may be need to lint test/**/*.test.ts but no need to emit to js.
+    // @see https://github.com/typescript-eslint/typescript-eslint/issues/890
+    project: './tsconfig.json',
+    sourceType: 'module',
+    typeScript: true,
+    ecmaVersion: 2019,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  rules: {
+    // see https://github.com/eslint/eslint/issues/6274
+    'generator-star-spacing': 'off',
+    'babel/generator-star-spacing': 'off',
+    semi: 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+  },
+}
---
+++ app/web/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // testMatch: ['app/web/**/?(*.)(spec|test|e2e).(j|t)s?(x)'],
+  testMatch: ['**/src/components/Artist/__tests__/index.tsx.test.ts'],
+}
