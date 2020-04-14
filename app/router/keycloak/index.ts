e4557a71d4a7b99bf20b108d3ce8b93d3bad4ec8
Jeff Tian <Jeff.Tian@externals.adidas.com>
2020-04-14 09:55:36 +0800

    show token under checksso routers

--- app/router/keycloak/index.ts
+++ app/router/keycloak/index.ts
@@ -1,13 +1,20 @@
-import { Application } from 'egg';
+import { Application } from 'egg'
 
 export default (app: Application) => {
-  const { router } = app;
+  const { router } = app
 
-  const subRouter = router.namespace('/keycloak');
+  const subRouter = router.namespace('/keycloak')
 
-  subRouter.get('keycloak.login', '/login', app.keycloak.protect(), async ctx => {
-    ctx.body = 'Keycloak login...';
-  });
+  subRouter.get(
+    'keycloak.login',
+    '/login',
+    app.keycloak.protect(),
+    async ctx => {
+      ctx.body = 'Keycloak login...'
+    },
+  )
 
-  subRouter.get('keycloak.*', '/*', app.keycloak.checkSso(), async ctx => { ctx.body = 'Keycloak resource.' });
-};
+  subRouter.get('keycloak.*', '/*', app.keycloak.checkSso(), async ctx => {
+    ctx.body = 'Keycloak resource. ' + ctx.session['keycloak-token']
+  })
+}
